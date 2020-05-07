b5b697d4859f91f70e5a429be49af3cc5f403958
Puentnuar <Puentnuar@users.noreply.github.com>
2020-05-07 13:56:10 +0400

    correct mistake in directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import HomePage from '../menu-item/menu-item.component'
+import MenuItem from '../menu-item/menu-item.component'
 
 import './directory.styles.scss'
 
@@ -49,7 +49,7 @@ class Directory extends React.Component {
   return (
    <div className='directory-menu'>
     {this.state.sections.map(({ title, id, imageUrl, size }) => (
-     <HomePage key={id} title={title} imageUrl={imageUrl} size={size} />
+     <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
     ))}
    </div>
   )
