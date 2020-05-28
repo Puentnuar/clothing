7b19735545cdb0dfb3158742a1e7eb38218c2363
Puentnuar <Puentnuar@users.noreply.github.com>
2020-05-28 17:29:52 +0400

    adding collection-previev selector form map for objact changing to array

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 import './collections-overview.styles.scss'
 
@@ -17,7 +17,7 @@ const CollectionsOvervies = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
- collections: selectCollections,
+ collections: selectCollectionsForPreview,
 })
 
 export default connect(mapStateToProps)(CollectionsOvervies)
